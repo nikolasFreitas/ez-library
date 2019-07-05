ab49cf2a4d69afc4487f2f576780d9afeb9a4cfa
Nikolas Freitas <nikolasbvf@gmail.com>
2019-07-05 00:07:58 -0300

    :tada:

---
+++ src/Components/Header/index.js
@@ -0,0 +1,8 @@
+import React from "React";
+import * as Styled from 'styled-components';
+
+const Header = () => (
+
+);
+
+export default Header;
---
+++ src/Components/Header/style.js
@@ -0,0 +1,7 @@
+import Styled from 'styled-components';
+
+export const Container = Styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+`;
