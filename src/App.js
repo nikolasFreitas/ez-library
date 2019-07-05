7eeb41ae116b023910542c28ae16ba3bb8c4d8a0
Nikolas Freitas <nikolasbvf@gmail.com>
2019-07-05 01:08:54 -0300

    :sparkles: add router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import { BrowserRouter as Router, Route, Switch, Redirect} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import Header from './Components/Header';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-      </header>
+      <Header />
+      <h1>OIEEE</h1>
       <Router>
         <Switch>
 
