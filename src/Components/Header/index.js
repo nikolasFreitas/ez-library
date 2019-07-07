fa7b24fcc5db2045084deeef18d0e30bec76466f
Nikolas Freitas <nikolasbvf@gmail.com>
2019-07-07 00:24:45 -0300

    :sparkles: Make header structure and router logic

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,8 +1,31 @@
-import React from "React";
-import * as Styled from 'styled-components';
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+import * as Styled from './style';
 
-const Header = () => (
+const Header = ({ location }) => {
+  return (
+    <Styled.Container>
+      <Styled.UpperHeadBox>
+        <Styled.Box>
+          <Styled.Title to="/">Ez Library</Styled.Title>
+        </Styled.Box>
+      </Styled.UpperHeadBox>
+      {location.pathname != '/' && (
+        <Styled.NavBarContainer>
+          <Styled.Box>
+            <Styled.NavBarBox>
+              <Styled.NavLink to="teste" selected={location.pathname === '/teste'}>
+                Teste
+              </Styled.NavLink>
+              <Styled.NavLink to="get" selected={location.pathname === '/get'}>
+                get
+              </Styled.NavLink>
+            </Styled.NavBarBox>
+          </Styled.Box>
+        </Styled.NavBarContainer>
+      )}
+    </Styled.Container>
+  );
+};
 
-);
-
-export default Header;
+export default withRouter(Header);
