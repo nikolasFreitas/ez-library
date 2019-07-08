0d91f1ef053f3fd8325d447cb85e0c498cd6e974
Nikolas Freitas <nikolasbvf@gmail.com>
2019-07-08 02:40:44 -0300

    :sparkles: Add config to call firebase

---
+++ src/config/firebase.js
@@ -0,0 +1,11 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDR_dve9HorNyOu4mIHSyjHUo7EQzpDFPo",
+  authDomain: "library-69dac.firebaseapp.com",
+  databaseURL: process.env.REACT_APP_FIREBASE_URL,
+  projectId: "library-69dac",
+  storageBucket: "",
+  messagingSenderId: "15293707504",
+  appId: "1:15293707504:web:7e49fb7417105962"
+}
+
+export const firebaseAuth = "72DPpwauP7fMUouoM1eObFePKJR3UBt5ca3aj2E4";
