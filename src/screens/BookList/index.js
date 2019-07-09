0f576adc5ef9533847440a436fbc482dbce7d58c
Nikolas Freitas <nikolasbvf@gmail.com>
2019-07-09 01:24:20 -0300

    :sparkles: :recycle: transformToComponent handle better data and not change reference

--- src/screens/BookList/index.js
+++ src/screens/BookList/index.js
@@ -3,16 +3,17 @@ import PropTypes from 'prop-types';
 import Select from 'react-select';
 import * as Styled from './style';
 import { book_options, transformToComponent } from './helpers';
+import { ReserveBook } from './components';
 import Table from '../../Components/Table';
 import Loader from '../../Components/Loader';
 import firebase from '../../services/requests';
 
-const data_enum = ['label', 'dayOfLoan', 'devolutionDate', 'returned'];
+const data_enum = ['label', 'dayOfLoan', 'devolutionDate', 'returned_component'];
 const header = ['Nome do livro', 'Data de ínicio de empréstimo', 'Data de devolução', 'Disponível'];
 
 const BookList = memo(() => {
   const [select, setSelect] = useState(false);
-  const [options, setOptions] = useState();
+  const [options, setOptions] = useState(false);
 
   useEffect(() => {
     firebase
@@ -23,8 +24,7 @@ const BookList = memo(() => {
           transformToComponent(
             result,
             <Styled.RentedText>Alugado</Styled.RentedText>,
-            <Styled.AvailableText>Disponível</Styled.AvailableText>,
-            'returned'
+            <Styled.AvailableText>Disponível</Styled.AvailableText>
           )
         );
       })
@@ -36,7 +36,15 @@ const BookList = memo(() => {
       <Styled.BoxContent>
         {options ? (
           <>
-            <Select options={book_options} onChange={setSelect} value={select} />
+            <Select options={options} onChange={setSelect} value={select} />
+            {select && (
+              <ReserveBook
+                header={header}
+                option={select}
+                data_enum={data_enum}
+                onCLick={setSelect}
+              />
+            )}
             <Styled.Title>Nosso acervo</Styled.Title>
             <Styled.TableWrapper>
               <Table header={header} data={options} data_enum={data_enum} onCLick={setSelect} />
