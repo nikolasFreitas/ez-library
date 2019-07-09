f0e7f0d30e2ddff86d1df5b1511ae24f78c15ab2
Nikolas Freitas <nikolasbvf@gmail.com>
2019-07-08 21:37:45 -0300

    :heavy_plus_sign: Add firebase lib

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,11 +1,17 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyDR_dve9HorNyOu4mIHSyjHUo7EQzpDFPo",
-  authDomain: "library-69dac.firebaseapp.com",
-  databaseURL: process.env.REACT_APP_FIREBASE_URL,
-  projectId: "library-69dac",
-  storageBucket: "",
-  messagingSenderId: "15293707504",
-  appId: "1:15293707504:web:7e49fb7417105962"
-}
+import * as firebase from 'firebase';
 
-export const firebaseAuth = "72DPpwauP7fMUouoM1eObFePKJR3UBt5ca3aj2E4";
+const firebaseConfig = {
+  apiKey: 'AIzaSyDR_dve9HorNyOu4mIHSyjHUo7EQzpDFPo',
+  authDomain: 'library-69dac.firebaseapp.com',
+  databaseURL: process.env.REACT_APP_FIREBASE_URL,
+  projectId: 'library-69dac',
+  storageBucket: '',
+  messagingSenderId: '15293707504',
+  appId: '1:15293707504:web:7e49fb7417105962',
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export const firebaseDb = firebase.database();
+
+export const firebaseAuth = '72DPpwauP7fMUouoM1eObFePKJR3UBt5ca3aj2E4';
